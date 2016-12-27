64f9c064c2eecdbc78602b9f9ee959f46b8b7fcc
root <root@webserver>
2016-12-28 00:54:31 +0100

    DEPLOY CHANGFES

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :user, "albert"
+set :user, "alejandro"
 set :rails_env, "production"
-server "sonohara.donmai.us", :roles => %w(web app db), :primary => true, :user => "albert"
-server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
+server "danbooru.deltaruby", :roles => %w(web app db), :primary => true, :user => "alejandro"
+#server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
