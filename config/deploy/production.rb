7e1e66647d9568d75f6d80f94ffe5f2ce6b2406a
Alejandro GR <alejandro@debmaster>
2017-01-01 04:44:01 +0100

    Another deployment change xd

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :user, "deltaruby"
+set :user, "danbooru"
 set :rails_env, "production"
-server "danbooru.deltaruby", :roles => %w(web app db), :primary => true, :user => "deltaruby"
+server "danbooru.deltaruby", :roles => %w(web app db), :primary => true, :user => "danbooru"
 #server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
