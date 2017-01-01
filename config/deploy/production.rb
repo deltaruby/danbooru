457e90c6580c129d4daf5c00ecda1b0a0a11f4c5
Alejandro GR <alejandro@debmaster>
2017-01-01 04:38:30 +0100

    Deployment changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :user, "alejandro"
+set :user, "deltaruby"
 set :rails_env, "production"
-server "danbooru.deltaruby", :roles => %w(web app db), :primary => true, :user => "alejandro"
+server "danbooru.deltaruby", :roles => %w(web app db), :primary => true, :user => "deltaruby"
 #server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
