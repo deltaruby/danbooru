14e26d08c0fc1efca1aa489f625419f5ca79c058
Alejandro GR <alejandro@debmaster>
2017-01-01 04:50:04 +0100

    Another change on users

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -9,7 +9,7 @@ timeout 180
 listen "/tmp/.unicorn.sock", :backlog => 256
 
 # Spawn unicorn master worker for user apps (group: apps)
-user 'deltaruby', 'deltaruby'
+user 'danbooru', 'danbooru'
 
 # Fill path to your app
 working_directory app_path
