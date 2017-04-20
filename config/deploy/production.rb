86a1032c26fdd99b376ccf37afcae84cc377abcb
ronin <ronin@debian>
2017-04-21 00:22:23 +0200

    Deploy changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :user, "danbooru"
 set :rails_env, "production"
-server "danbooru.deltaruby", :roles => %w(web app db), :primary => true, :user => "danbooru"
+server "danbooru_test.rnn", :roles => %w(web app db), :primary => true, :user => "danbooru"
 #server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
